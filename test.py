3a84d813415efe46d301299a65c1253d8715b79d
Daniel <129753283+Flqmmable@users.noreply.github.com>
2023-04-03 20:09:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ from scapy.layers.http import HTTPResponse
 
 
 def process_packet(packet):
-    if packet.haslayer(HTTPResponse):
-        httpData = packet[TCP].payload
-        httpData = str(httpData)
-        if '200' in httpData:
-            htmlData = packet[HTTPResponse][Raw].load
+    if packet.haslayer(HTTPResponse): # Check if packet is a HTTP Response
+        httpData = packet[TCP].payload # Store HTTP information
+        httpData = str(httpData) 
+        if '200' in httpData: # Check if HTTP contains 200 OK Status
+            htmlData = packet[HTTPResponse][Raw].load # Extract HTML information
             print(htmlData)
             
-sniff(filter='port 80', prn=process_packet)
+sniff(filter='port 80', prn=process_packet) # Sniff Packet with Port 80 (HTTP)
